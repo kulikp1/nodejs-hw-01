f5d8ad672fa268f9413fd84fa6f8b26ca1337968
kulikp1 <pashakylik2@gmail.com>
2024-07-24 23:47:28 +0300

    step 4

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
 
-export const getAllContacts = async () => {};
+export const getAllContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contactsList = JSON.parse(data);
+    return contactsList;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 console.log(await getAllContacts());
