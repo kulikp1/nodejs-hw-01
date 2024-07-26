837875494bb968309cd3c387e47afe175a361d97
kulikp1 <pashakylik2@gmail.com>
2024-07-26 18:39:43 +0300

    step 5,6,7

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,18 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
 
-export const removeLastContact = async () => {};
+export const removeLastContact = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contactsList = JSON.parse(data);
+
+    if (!contactsList.length) return;
+
+    contactsList.pop();
+    fs.writeFile(PATH_DB, JSON.stringify(contactsList, undefined, 2));
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 removeLastContact();
